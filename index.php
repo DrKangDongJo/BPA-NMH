eb2107836d6daf95eab24bee9eba2848695cd14a
jakesebastian-forbes <jforbes557@gmail.com>
2023-06-21 04:11:43 +0800

    Initial Commit - UI - WIP

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Building Permit Application | Home</title>
+
+
+    <style>
+        .bg{
+            background-image: url("img/construction_bg.webp");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .login-cont{
+            background-color: #5A8BB5;
+            width: 400px;
+            height: 600px;
+            border-radius: 10px;
+            position:absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding:2%;
+
+        }    
+    </style>
+
+    <link rel="stylesheet" href="bootstrap-5.3.0\css\bootstrap.css">
+    <link rel="stylesheet" href="css/general.css">
+</head>
+<body class = "bg">
+    
+<div class="container-fluid">
+<!-- ** NAVBAR -->
+    <div class="row">
+    <?php
+    require 'components\navbar.php';
+    ?>
+    </div>
+   
+    <div class="login-cont">
+       <div class="row mt-5" style = "justify-content:center">
+            <img src="img/Seal_of_Nasugbu.png" alt="Seal of Nasugbu" style="width:50%;" class = "img-fluid">
+       </div>
+        <br>
+
+       <div class="row">
+        <label for="USERNAME" class = "white-text ">USERNAME</label>
+        <div class="row m-auto">
+            <input type="text" value="" name="USERNAME" 
+            class="my-upper my-text-input my-border-8" placeholder="username88" required>
+        </div>
+        <label for="PASSWORD" class = "white-text ">PASSWORD</label>
+        <div class="row m-auto">
+            <input type="password" value="" name="PASSWORD" 
+            class="my-upper my-text-input my-border-8" placeholder="required" required>
+        </div>
+       </div>
+
+       <div class= "my-upper white-text" style="font-size: 0.9rem;">
+       <p class = "mb-0 mt-1">FORGOT PASSWORD?</p>
+       <p class = "mb-1">SIGNUP</p>
+
+       </div>
+       
+        <a href = "pages/applicant/applicant_home.php">
+            <button type="" class="btn my-btn-blue mt-0 float-end px-4" style="color:white;">
+            LOGIN</button>
+       </a>
+      
+    </div>
+
+
+
+</div>
+
+
+
+
+
+
+
+<script>
+</script>
+
+</body>
+</html>
---
+++ pages/applicant_home.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Applicant Home</title>
+    <link rel="stylesheet" href="../bootstrap-5.3.0\css\bootstrap.css">
+    <link rel="stylesheet" href="../css/general.css">
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <?php
+        require '../components/navbar.php';
+        ?>
+<div class="custom-control custom-switch">
+  <input type="checkbox" class="custom-control-input" id="customSwitches" onclick="check_state()">
+  <label class="custom-control-label" for="customSwitches">Toggle this to switch view(demo only)</label>
+</div>
+    </div>
+
+    <!-- ** first time use! -->
+    <div id = "first_time_use" class="row text-center" style="margin:2%">
+        <p class ="muted">VIEW OF FIRST TIME USER</p>
+        <p class ="m-2">No recent or saved Projects found.</p>
+        <p  class ="m-2">Create and add one now.</p>
+        <button type="button" class = "btn my-btn-blue mx-auto white-text" style="width:auto"
+        data-bs-toggle="modal" data-bs-target="#project_template_select_modal">CREATE PROJECT</button>
+    </div>
+
+</div>
+
+
+<!-- **! MODAL -->
+
+<!-- Modal -->
+<div class="modal fade" id="project_template_select_modal" tabindex="-1" aria-labelledby="project_template_select_modal_Label" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="project_template_select_modal_Label">Project Templates</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body" >
+        
+        <div style = "width:200px;aspect-ratio:1/1;border:1px solid black;">
+
+            <div style = "height:80%;border-bottom:1px solid black;"></div>
+           
+           <a href="applicant_openProject.php">
+            <p class="text-center">BUILDING PERMIT APPLICATION</p>
+            </a>
+
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn my-btn-red" data-bs-dismiss="modal">Close</button>
+        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div id="with_saved_project" class ="p-4" hidden>
+    <div class="row">
+        <p>Recent Projects</p>
+    </div>
+    <div class="row ms-4">
+        <div style = "width:245px;height:180px;border:1px solid black;">
+
+        <div style = "height:80%;border-bottom:1px solid black;"></div>
+        <p class="text-center">2nd floor renovation</p>
+
+</div>
+
+    </div>
+
+</div>
+
+
+
+<script src="../bootstrap-5.3.0/js/bootstrap.js"></script>
+<script src="../dependecies/jquery-3.6.4.js"></script>
+<script>
+
+
+function check_state(){
+    if($("#customSwitches").is(':checked') == true){
+        $("#first_time_use").attr('hidden','hidden')
+    $("#with_saved_project").removeAttr('hidden')
+   
+}else{
+    $("#first_time_use").removeAttr('hidden');
+    $("#with_saved_project").attr('hidden','hidden')
+    
+  
+
+}
+}
+
+
+
+
+</script>
+
+</body>
+</html>
