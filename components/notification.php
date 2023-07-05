4a7111d8722587d07ebdae6f1ecf899a452c47f6
komisan14 <rodriguezreignoel@gmail.com>
2023-07-05 16:50:27 +0800

    initial notification

---
+++ components/notification.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap-5.3.0\css\bootstrap.css">
+    <link rel="stylesheet" href="../css/general.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <?php
+            require '../components/navbar.php';
+            ?>
+        </div>
+    </div>
+</body>
+</html>
