9bd2c324afe0a3e4ad20b5b266b4c1771d9ce38d
jakesebastian-forbes <jforbes557@gmail.com>
2023-07-18 22:27:40 +0800

    a

--- php_func/login_validation.php
+++ php_func/login_validation.php
@@ -8,53 +8,63 @@ $password = $_POST['PASSWORD'];
 
 //call select function from db_func
 $condition = "username = '$username'";
-$result = select("applicant","$condition");
 
 //check admin
-
+$result = select("admin", "$condition");
 
 //check if there are result
-if (mysqli_num_rows($result) > 0) {   
-while($row = mysqli_fetch_assoc($result)) {
-    // echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["password"]. "<br>";
-    $db_username = $row['username'];
-    $db_pass = $row['password'];
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"] . " - Name: " . $row["username"] . " " . $row["password"] . "<br>";
+        // $db_username = $row['username'];
+        $db_pass = $row['password'];
 
-    
-// verify password
-// echo "verifying";
-if( password_verify($password,$db_pass) == 1){
+        $department = strtolower($row['department']);
+        // verify password
 
-    //create session
-    session_start();
-    $_SESSION['user_id'] = $row['id'];
-    $_SESSION['username'] = $row['username'];
-    $_SESSION['firstname'] = $row['firstname'];
-    $_SESSION['middlename'] = $row['middlename'];
-    $_SESSION['lastname'] = $row['lastname'];
-    $_SESSION['contact_no'] = $row['contact_no'];
-    $_SESSION['privilege'] = 'applicant';
+        if (password_verify($password, $db_pass) == 1) { //success
 
+            //create session
+            session_start();
+            $_SESSION['user_id'] = $row['id'];
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['privilege'] = 'admin';
 
-   header('Location: ../pages/applicant_home.php');
-}else{
-   header('Location: ../index.php?error=incorrect,username='.$username);
-}
+            header('Location: ../pages/' . $department . '_home.php');
+        } else { //wrong username/password
+            header('Location: ../index.php?error=incorrect,username=' . $username);
+        }
+    }
+} else { //no result
 
-  }
+    //check applicant
+    $result = select("applicant", "$condition");
+    //check if there are result
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "id: " . $row["id"] . " - Name: " . $row["username"] . " " . $row["password"] . "<br>";
+            // $db_username = $row['username'];
+            $db_pass = $row['password'];
 
+            // $department = strtolower($row['department']);
+            // verify password
 
-} else {
+            if (password_verify($password, $db_pass) == 1) { //success
 
-    //check admin
+                //create session
+                session_start();
+                $_SESSION['user_id'] = $row['id'];
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['privilege'] = 'applicant';
 
-    //else return no data
-    // echo "0 results";
-    header('Location: ../index.php?error=noaccount');
+                header('Location: ../pages/applicant_home.php');
+            } else { //wrong username/password
+                header('Location: ../index.php?error=incorrect,username=' . $username);
+            }
+        }
+    } else {
+        header('Location: ../index.php?error=noaccount');
     }
 
 
-
-
-
-?>
+}
